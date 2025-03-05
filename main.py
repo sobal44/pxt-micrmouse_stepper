d75292147a6ebf379c854435cdaae7f523ee255e
sobal44 <sobal44@wp.pl>
2025-03-05 21:32:02 +0100

    Wybor targetu z 5x5 array i zapisanie go w zmiennych Labirynt_X i Labirynt_Y

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def Odczyt_czujnikow_bocznych():
-    pass
+def Odczyt_czujnikow():
+    global Czujnik_Prawy, Czujnik_Lewy, Czujnik_Przod
+    pins.digital_write_pin(DigitalPin.P0, 1)
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    Rangefinder.init()
+    Czujnik_Prawy = Rangefinder.distance()
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    pins.digital_write_pin(DigitalPin.P1, 1)
+    Rangefinder.init()
+    Czujnik_Lewy = Rangefinder.distance()
+    Czujnik_Przod = makerbit.get_ultrasonic_distance(DistanceUnit.CM)
 
 def on_button_pressed_a():
-    pass
+    Przeszukanie_Labiryntu()
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    sobal.Silnik_Krokowy_Obrot(sobal.Dir.CW, 90, 175, 3050, 55.7)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def Przeszukanie_Labiryntu():
+    global Labirynt
+    Labirynt = [[0, 0, 0, 0, 0, 0, 0], [0], [0], [0]]
+    sobal.Silnik_Krokowy_Ruch_na_odleglosc(sobal.Dir.CW, 450, 175, 3500, 55.7)
+    sobal.Silnik_Krokowy_Ruch_na_odleglosc(sobal.Dir.CW, 180, 175, 3500, 55.7)
+    Odczyt_czujnikow()
+Labirynt: List[List[number]] = []
+Czujnik_Przod = 0
+Czujnik_Lewy = 0
+Czujnik_Prawy = 0
+makerbit.connect_ultrasonic_distance_sensor(DigitalPin.P15, DigitalPin.P16)
+
+def on_forever():
+    Odczyt_czujnikow()
+    basic.show_icon(IconNames.HEART)
+    basic.show_number(Czujnik_Lewy)
+    basic.show_number(Czujnik_Prawy)
+    basic.show_number(Czujnik_Przod)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -10,19 +10,22 @@ function Odczyt_czujnikow () {
     Czujnik_Przod = makerbit.getUltrasonicDistance(DistanceUnit.CM)
 }
 function Przeszukanie_Labiryntu () {
+    Labirynt = []
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(
     sobal.Dir.CW,
-    450,
+    45,
     175,
     3500,
     55.7
     )
-    Odczyt_czujnikow()
-}
-input.onButtonPressed(Button.A, function () {
-    Przeszukanie_Labiryntu()
-})
-input.onButtonPressed(Button.B, function () {
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    sobal.Silnik_Krokowy_Ruch_na_odleglosc(
+    sobal.Dir.CW,
+    180,
+    175,
+    3500,
+    55.7
+    )
     sobal.Silnik_Krokowy_Obrot(
     sobal.Dir.CW,
     90,
@@ -30,15 +33,62 @@ input.onButtonPressed(Button.B, function () {
     3050,
     55.7
     )
+    Odczyt_czujnikow()
+}
+input.onButtonPressed(Button.A, function () {
+    if (Set_Up_Target == 0) {
+        Labirynt_X += 1
+        if (Labirynt_X >= 5) {
+            Labirynt_X = 0
+        }
+        for (let indeks = 0; indeks <= 4; indeks++) {
+            for (let index2 = 0; index2 <= 4; index2++) {
+                led.unplot(indeks, index2)
+            }
+        }
+    }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Przeszukanie_Labiryntu()
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Set_Up_Target == 0) {
+        Set_Up_Target = 1
+        basic.showString("X")
+        basic.showNumber(Labirynt_X)
+        basic.showString("Y")
+        basic.showNumber(Labirynt_Y)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Set_Up_Target == 0) {
+        Labirynt_Y += 1
+        if (Labirynt_Y >= 5) {
+            Labirynt_Y = 0
+        }
+        for (let indeks = 0; indeks <= 4; indeks++) {
+            for (let index2 = 0; index2 <= 4; index2++) {
+                led.unplot(indeks, index2)
+            }
+        }
+    }
+})
+let Labirynt: number[] = []
 let Czujnik_Przod = 0
 let Czujnik_Lewy = 0
 let Czujnik_Prawy = 0
+let Labirynt_Y = 0
+let Labirynt_X = 0
+let Set_Up_Target = 0
+motor.motorStopAll()
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P15, DigitalPin.P16)
+Set_Up_Target = 0
+Labirynt_X = 0
+Labirynt_Y = 0
+basic.showString("TARGET")
 basic.forever(function () {
-    Odczyt_czujnikow()
-    basic.showIcon(IconNames.Heart)
-    basic.showNumber(Czujnik_Lewy)
-    basic.showNumber(Czujnik_Prawy)
-    basic.showNumber(Czujnik_Przod)
+    if (Set_Up_Target == 0) {
+        led.plot(4 - Labirynt_X, 4 - Labirynt_Y)
+    }
 })
