150cdb7f9abb637390b1cfd43ecf5247d5cf6264
sobal44 <sobal44@wp.pl>
2025-03-09 19:15:27 +0100

    Troche pozmieniane, update scianeg jest chyba ok, teraz walcze z algorytmem do updateu wylania wody na podstawie scianek

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def Odczyt_czujnikow():
     Rangefinder.init()
     Czujnik_Lewy = Rangefinder.distance()
     Czujnik_Przod = makerbit.get_ultrasonic_distance(DistanceUnit.CM)
+
+def on_sound_loud():
+    global lista
+    lista = [0, 1]
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
 def Kalibracja_o_tylnia_sciane():
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(sobal.Dir.CW, 55, 175, 3500, 55.7)
     Odczyt_czujnikow()
@@ -24,6 +30,13 @@ def on_button_pressed_a():
                 led.unplot(idex_X, index2)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def Wgrani_labiryntu_bez_scian(Ilosc_komorek_w_labiryncie: number):
+    global Sciany_Labiryntu
+    Sciany_Labiryntu = []
+    j = 0
+    while j <= Ilosc_komorek_w_labiryncie:
+        Sciany_Labiryntu.append(0)
+        j += 1
 def Ruch_Lewo():
     global Orientacja_Robota
     sobal.Silnik_Krokowy_Obrot(sobal.Dir.CCW, 90, 175, 3050, 55.7)
@@ -33,30 +46,33 @@ def Ruch_Lewo():
             Orientacja_Robota = 2
 
 def on_logo_pressed():
-    Wylanie_Wody()
+    Wylanie_Wody(Wilekosc_Labiryntu_X, Wielkosc_Labiryntu_Y2)
     Przeszukanie_Labiryntu()
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-def Wylanie_Wody():
+def Wylanie_Wody(Wielkosc_Lbairyntu_X: number, Wielkosc_Labiryntu_Y: number):
     global Labirynt
     Labirynt = []
     Index_Y = 0
     while Index_Y <= Wielkosc_Labiryntu_Y:
         idex_X2 = 0
-        while idex_X2 <= Wilekosc_Labiryntu_X:
+        while idex_X2 <= Wielkosc_Lbairyntu_X:
             Labirynt.append(abs(idex_X2 - Labirynt_X) + abs(Index_Y - Labirynt_Y))
             idex_X2 += 1
         Index_Y += 1
     music.play(music.tone_playable(131, music.beat(BeatFraction.BREVE)),
         music.PlaybackMode.UNTIL_DONE)
     serial.write_line("Wylano Wode")
-    i = 0
+    wysłanie_Tablicy_przez_port_szeregowy(Labirynt)
+    return Labirynt
+def wysłanie_Tablicy_przez_port_szeregowy(array: List[any]):
+    global i
     while i <= Wilekosc_Labiryntu_X:
-        serial.write_numbers([Labirynt[5 * abs(4 - i) + 0],
-                Labirynt[5 * abs(4 - i) + 1],
-                Labirynt[5 * abs(4 - i) + 2],
-                Labirynt[5 * abs(4 - i) + 3],
-                Labirynt[5 * abs(4 - i) + 4]])
+        serial.write_numbers([array[5 * abs(4 - i) + 0],
+                array[5 * abs(4 - i) + 1],
+                array[5 * abs(4 - i) + 2],
+                array[5 * abs(4 - i) + 3],
+                array[5 * abs(4 - i) + 4]])
         i += 1
 
 def on_button_pressed_ab():
@@ -69,6 +85,8 @@ def on_button_pressed_ab():
         basic.show_number(Labirynt_Y)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+def Update_Scian_Labiryntu():
+    pass
 def Ruch_Obrot_Prawo():
     global Orientacja_Robota
     sobal.Silnik_Krokowy_Obrot(sobal.Dir.CW, 90, 175, 3050, 55.7)
@@ -91,73 +109,101 @@ def Ruch_Przod():
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(sobal.Dir.CW, 180, 175, 3500, 55.7)
     music.play(music.tone_playable(587, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
-    basic.show_number(połozenie_w_labiryncie)
+    basic.show_number(polozenie_w_labiryncie)
     Odczyt_czujnikow()
+
+def on_gesture_shake():
+    motor.motor_stop_all()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def Przeszukanie_Labiryntu():
-    global połozenie_w_labiryncie
+    global polozenie_w_labiryncie
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(sobal.Dir.CW, 45, 175, 3500, 55.7)
     Ruch_Przod()
-    połozenie_w_labiryncie = połozenie_w_labiryncie + 5
-    while Labirynt[połozenie_w_labiryncie] != 0:
+    polozenie_w_labiryncie = polozenie_w_labiryncie + 5
+    while Labirynt[polozenie_w_labiryncie] != 0:
         if Orientacja_Robota == 0:
-            if Labirynt[połozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 5
+            if Labirynt[polozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 5
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie + 1] < Labirynt[połozenie_w_labiryncie] and Czujnik_Prawy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 1
+            elif Labirynt[polozenie_w_labiryncie + 1] < Labirynt[polozenie_w_labiryncie] and Czujnik_Prawy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 1
                 Ruch_Obrot_Prawo()
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie - 1] < Labirynt[połozenie_w_labiryncie] and Czujnik_Lewy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 1
+            elif Labirynt[polozenie_w_labiryncie - 1] < Labirynt[polozenie_w_labiryncie] and Czujnik_Lewy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 1
+                Ruch_Lewo()
+                Ruch_Przod()
+            else:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 5
+                Ruch_Lewo()
                 Ruch_Lewo()
                 Ruch_Przod()
         elif Orientacja_Robota == 1:
-            if Labirynt[połozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and Czujnik_Lewy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 5
+            if Labirynt[polozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and Czujnik_Lewy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 5
                 Ruch_Lewo()
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie + 1] < Labirynt[połozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 1
+            elif Labirynt[polozenie_w_labiryncie + 1] < Labirynt[polozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 1
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and Czujnik_Prawy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 5
+            elif Labirynt[polozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and Czujnik_Prawy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 5
+                Ruch_Obrot_Prawo()
+                Ruch_Przod()
+            else:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 1
+                Ruch_Obrot_Prawo()
                 Ruch_Obrot_Prawo()
                 Ruch_Przod()
         elif Orientacja_Robota == -1:
-            if Labirynt[połozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and Czujnik_Prawy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 5
+            if Labirynt[polozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and Czujnik_Prawy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 5
                 Ruch_Obrot_Prawo()
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie - 1] < Labirynt[połozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 1
+            elif Labirynt[polozenie_w_labiryncie - 1] < Labirynt[polozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 1
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and Czujnik_Lewy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 5
+            elif Labirynt[polozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and Czujnik_Lewy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 5
+                Ruch_Lewo()
+                Ruch_Przod()
+            else:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 1
+                Ruch_Lewo()
                 Ruch_Lewo()
                 Ruch_Przod()
         elif Orientacja_Robota == 2:
-            if Labirynt[połozenie_w_labiryncie - 1] < Labirynt[połozenie_w_labiryncie] and Czujnik_Prawy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 1
+            if Labirynt[polozenie_w_labiryncie - 1] < Labirynt[polozenie_w_labiryncie] and Czujnik_Prawy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 1
                 Ruch_Obrot_Prawo()
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[połozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
-                połozenie_w_labiryncie = połozenie_w_labiryncie - 5
+            elif Labirynt[polozenie_w_labiryncie - (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] and makerbit.get_ultrasonic_distance(DistanceUnit.CM) > 5:
+                polozenie_w_labiryncie = polozenie_w_labiryncie - 5
                 Ruch_Przod()
-            elif Labirynt[połozenie_w_labiryncie + 1] < Labirynt[połozenie_w_labiryncie] and Czujnik_Lewy > 60:
-                połozenie_w_labiryncie = połozenie_w_labiryncie + 1
+            elif Labirynt[polozenie_w_labiryncie + 1] < Labirynt[polozenie_w_labiryncie] and Czujnik_Lewy > 60:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 1
                 Ruch_Lewo()
+                Ruch_Przod()
+            else:
+                polozenie_w_labiryncie = polozenie_w_labiryncie + 5
+                Ruch_Obrot_Prawo()
+                Ruch_Obrot_Prawo()
                 Ruch_Przod()
     music._play_default_background(music.built_in_playable_melody(Melodies.FUNK),
         music.PlaybackMode.IN_BACKGROUND)
     basic.show_string("P")
-    basic.show_number(połozenie_w_labiryncie)
-połozenie_w_labiryncie = 0
+    basic.show_number(polozenie_w_labiryncie)
+polozenie_w_labiryncie = 0
+i = 0
 Labirynt: List[number] = []
+lista: List[number] = []
 Czujnik_Przod = 0
 Czujnik_Lewy = 0
 Czujnik_Prawy = 0
+Sciany_Labiryntu: List[number] = []
 Orientacja_Robota = 0
-Wielkosc_Labiryntu_Y = 0
+Wielkosc_Labiryntu_Y2 = 0
 Wilekosc_Labiryntu_X = 0
 Labirynt_Y = 0
 Labirynt_X = 0
@@ -169,14 +215,18 @@ Set_Up_Target = 0
 Labirynt_X = 0
 Labirynt_Y = 0
 Wilekosc_Labiryntu_X = 4
-Wielkosc_Labiryntu_Y = 4
+Wielkosc_Labiryntu_Y2 = 4
 # Orientacja Robota wzgledem labiryntu
-# o = Przod 
+# o = Przod
 # 1 = Prawo
 # -1 = Lewo
 # 2 = Tył
 Orientacja_Robota = 0
+Ilosc_komorek_w_labiryncie2 = Wielkosc_Labiryntu_Y2 * Wilekosc_Labiryntu_X + (Wielkosc_Labiryntu_Y2 + Wilekosc_Labiryntu_X)
+Wgrani_labiryntu_bez_scian(Ilosc_komorek_w_labiryncie2)
 basic.show_string("TARGET")
+serial.write_line("Sciany Labiryntu")
+wysłanie_Tablicy_przez_port_szeregowy(Sciany_Labiryntu)
 
 def on_forever():
     if Set_Up_Target == 0:
--- main.ts
+++ main.ts
@@ -9,11 +9,15 @@ function Odczyt_czujnikow () {
     Czujnik_Lewy = Rangefinder.distance()
     Czujnik_Przod = makerbit.getUltrasonicDistance(DistanceUnit.CM)
 }
+input.onSound(DetectedSound.Loud, function () {
+    serial.writeNumber(9 & 4)
+    serial.writeLine("")
+})
 function Kalibracja_o_tylnia_sciane () {
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(
     sobal.Dir.CW,
     55,
-    175,
+    150,
     3500,
     55.7
     )
@@ -32,11 +36,34 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+function Wgrani_labiryntu_bez_scian (Ilosc_komorek_w_labiryncie: number) {
+    Sciany_Labiryntu.push(8)
+    while (l <= Ilosc_komorek_w_labiryncie - 1) {
+        Sciany_Labiryntu.push(0)
+        l += 1
+    }
+    for (let indeks = 0; indeks <= Wilekosc_Labiryntu_X; indeks++) {
+        Sciany_Labiryntu[indeks] = 8
+    }
+    for (let indeks2 = 0; indeks2 <= Wilekosc_Labiryntu_X; indeks2++) {
+        Sciany_Labiryntu[Wilekosc_Labiryntu_X + Wielkosc_Labiryntu_Y2 * Wilekosc_Labiryntu_X + indeks2] = 2
+    }
+    for (let indeks3 = 0; indeks3 <= Wielkosc_Labiryntu_Y2; indeks3++) {
+        Sciany_Labiryntu[(Wielkosc_Labiryntu_Y2 + 1) * indeks3] = 2
+    }
+    for (let indeks4 = 0; indeks4 <= Wielkosc_Labiryntu_Y2; indeks4++) {
+        Sciany_Labiryntu[(Wielkosc_Labiryntu_Y2 + 1) * indeks4 + Wielkosc_Labiryntu_Y2] = 4
+    }
+    Sciany_Labiryntu[0] = 9
+    Sciany_Labiryntu[Wilekosc_Labiryntu_X] = 12
+    Sciany_Labiryntu[Wielkosc_Labiryntu_Y2 * (Wilekosc_Labiryntu_X + 1)] = 3
+    Sciany_Labiryntu[Wilekosc_Labiryntu_X + Wielkosc_Labiryntu_Y2 + Wilekosc_Labiryntu_X * Wielkosc_Labiryntu_Y2] = 6
+}
 function Ruch_Lewo () {
     sobal.Silnik_Krokowy_Obrot(
     sobal.Dir.CCW,
     90,
-    175,
+    150,
     3050,
     55.7
     )
@@ -48,32 +75,33 @@ function Ruch_Lewo () {
     }
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Wylanie_Wody()
+    Wylanie_Wody(Wilekosc_Labiryntu_X, Wielkosc_Labiryntu_Y2)
+    wyslanie_Tablicy_przez_port_szeregowy(Labirynt)
     Przeszukanie_Labiryntu()
 })
-function Wylanie_Wody () {
-    let idex_X2: number;
-Labirynt = []
+function Wylanie_Wody (Wielkosc_Lbairyntu_X: number, Wielkosc_Labiryntu_Y: number) {
     while (Index_Y <= Wielkosc_Labiryntu_Y) {
         idex_X2 = 0
-        while (idex_X2 <= Wilekosc_Labiryntu_X) {
+        while (idex_X2 <= Wielkosc_Lbairyntu_X) {
             Labirynt.push(Math.abs(idex_X2 - Labirynt_X) + Math.abs(Index_Y - Labirynt_Y))
             idex_X2 += 1
         }
         Index_Y += 1
     }
-    music.play(music.tonePlayable(131, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(131, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     serial.writeLine("Wylano Wode")
-    while (i <= Wilekosc_Labiryntu_X) {
-        serial.writeNumbers([
-        Labirynt[5 * Math.abs(4 - i) + 0],
-        Labirynt[5 * Math.abs(4 - i) + 1],
-        Labirynt[5 * Math.abs(4 - i) + 2],
-        Labirynt[5 * Math.abs(4 - i) + 3],
-        Labirynt[5 * Math.abs(4 - i) + 4]
-        ])
-        i += 1
-    }
+    // Sciany W labiryncie
+    // 
+    // |1
+    // 
+    // _
+    // 2
+    // 
+    // 4|
+    // 
+    // 8
+    // -
+    wyslanie_Tablicy_przez_port_szeregowy(Labirynt)
 }
 input.onButtonPressed(Button.AB, function () {
     if (Set_Up_Target == 0) {
@@ -84,17 +112,170 @@ input.onButtonPressed(Button.AB, function () {
         basic.showNumber(Labirynt_Y)
     }
 })
+function Update_Scian_Labiryntu (Lewa: number, Przod: number, Prawa: number, Orientacja: number) {
+    if (Orientacja == 0) {
+        if (Przod < 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 7
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 5
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 6
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 4
+        }
+        if (Przod < 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 3
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 1
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 2
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 0
+        }
+    }
+    if (Orientacja == -1) {
+        if (Przod < 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 11
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 10
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 3
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 2
+        }
+        if (Przod < 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 9
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 8
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 1
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 0
+        }
+    }
+    if (Orientacja == 1) {
+        if (Przod < 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 14
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 10
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 12
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 8
+        }
+        if (Przod < 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 6
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 2
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 4
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 0
+        }
+    }
+    if (Orientacja == 2) {
+        if (Przod < 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 13
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 5
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 9
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa < 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 1
+        }
+        if (Przod < 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 12
+        }
+        if (Przod > 50 && (Lewa < 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 4
+        }
+        if (Przod < 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 8
+        }
+        if (Przod > 50 && (Lewa > 50 && Prawa > 50)) {
+            Sciany_Labiryntu[polozenie_w_labiryncie] = 0
+        }
+    }
+}
 function Ruch_Obrot_Prawo () {
     sobal.Silnik_Krokowy_Obrot(
     sobal.Dir.CW,
     90,
-    175,
+    150,
     3050,
     55.7
     )
     Orientacja_Robota = Orientacja_Robota + 1
     if (Orientacja_Robota > 2) {
         Orientacja_Robota = -1
+    }
+}
+// Zmienna Test jest tylko do sprawdzenia poprawnosci wylania wody
+function Update_wylania_wody () {
+    test = [
+    13,
+    13,
+    9,
+    10,
+    12,
+    3,
+    4,
+    5,
+    9,
+    6,
+    9,
+    6,
+    5,
+    3,
+    12,
+    3,
+    10,
+    6,
+    9,
+    6,
+    11,
+    10,
+    10,
+    2,
+    14
+    ]
+    Kolejka = []
+    Kolejka.push((1 + Labirynt_X) * Labirynt_Y + Labirynt_X)
+    while (false) {
+        // Sprawdzenie czy jest scianka na kierunku
+        if (test[Kolejka[0]] & 1) {
+        	
+        }
+        if (test[Kolejka[0]] & 2) {
+        	
+        }
+        if (test[Kolejka[0]] & 4) {
+        	
+        }
+        if (test[Kolejka[0]] & 8) {
+        	
+        }
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -114,7 +295,7 @@ function Ruch_Przod () {
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(
     sobal.Dir.CW,
     180,
-    175,
+    150,
     3500,
     55.7
     )
@@ -122,6 +303,19 @@ function Ruch_Przod () {
     basic.showNumber(polozenie_w_labiryncie)
     Odczyt_czujnikow()
 }
+function wyslanie_Tablicy_przez_port_szeregowy (array: number[]) {
+    for (let j = 0; j <= Wilekosc_Labiryntu_X; j++) {
+        for (let indeks5 = 0; indeks5 <= Wielkosc_Labiryntu_Y2; indeks5++) {
+            serial.writeNumber(array[(Wilekosc_Labiryntu_X + 1) * Math.abs(Wilekosc_Labiryntu_X - j) + indeks5])
+            serial.writeString(" ")
+        }
+        serial.writeLine("")
+    }
+    serial.writeLine("")
+}
+input.onGesture(Gesture.Shake, function () {
+    motor.motorStopAll()
+})
 function Przeszukanie_Labiryntu () {
     sobal.Silnik_Krokowy_Ruch_na_odleglosc(
     sobal.Dir.CW,
@@ -133,6 +327,19 @@ function Przeszukanie_Labiryntu () {
     Ruch_Przod()
     polozenie_w_labiryncie = polozenie_w_labiryncie + 5
     while (Labirynt[polozenie_w_labiryncie] != 0) {
+        Update_Scian_Labiryntu(Czujnik_Lewy, Czujnik_Przod, Czujnik_Prawy, Orientacja_Robota)
+        // Sciany W labiryncie
+        // 
+        // |1
+        // 
+        // _
+        // 2
+        // 
+        // 4|
+        // 
+        // 8
+        // -
+        wyslanie_Tablicy_przez_port_szeregowy(Sciany_Labiryntu)
         if (Orientacja_Robota == 0) {
             if (Labirynt[polozenie_w_labiryncie + (Wilekosc_Labiryntu_X + 1)] < Labirynt[polozenie_w_labiryncie] && makerbit.getUltrasonicDistance(DistanceUnit.CM) > 5) {
                 polozenie_w_labiryncie = polozenie_w_labiryncie + 5
@@ -212,18 +419,24 @@ function Przeszukanie_Labiryntu () {
     basic.showNumber(polozenie_w_labiryncie)
 }
 let polozenie_w_labiryncie = 0
-let i = 0
+let idex_X2 = 0
 let Index_Y = 0
-let Labirynt: number[] = []
+let l = 0
 let Czujnik_Przod = 0
 let Czujnik_Lewy = 0
 let Czujnik_Prawy = 0
+let Sciany_Labiryntu: number[] = []
+let Labirynt: number[] = []
 let Orientacja_Robota = 0
-let Wielkosc_Labiryntu_Y = 0
+let Wielkosc_Labiryntu_Y2 = 0
 let Wilekosc_Labiryntu_X = 0
 let Labirynt_Y = 0
 let Labirynt_X = 0
 let Set_Up_Target = 0
+let Kolejka: number[] = []
+let test: number[] = []
+let k = 0
+let lista: number[] = []
 motor.motorStopAll()
 serial.writeLine("MicroMouse")
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P15, DigitalPin.P16)
@@ -231,14 +444,31 @@ Set_Up_Target = 0
 Labirynt_X = 0
 Labirynt_Y = 0
 Wilekosc_Labiryntu_X = 4
-Wielkosc_Labiryntu_Y = 4
+Wielkosc_Labiryntu_Y2 = 4
 // Orientacja Robota wzgledem labiryntu
 // o = Przod
 // 1 = Prawo
 // -1 = Lewo
 // 2 = Tył
 Orientacja_Robota = 0
+let Ilosc_komorek_w_labiryncie2 = Wielkosc_Labiryntu_Y2 * Wilekosc_Labiryntu_X + (Wielkosc_Labiryntu_Y2 + Wilekosc_Labiryntu_X)
+Labirynt = []
+Sciany_Labiryntu = []
+Wgrani_labiryntu_bez_scian(Ilosc_komorek_w_labiryncie2)
 basic.showString("TARGET")
+serial.writeLine("Sciany Labiryntu")
+// Sciany W labiryncie
+// 
+// |1
+// 
+// _
+// 2
+// 
+// 4|
+// 
+// 8
+// -
+wyslanie_Tablicy_przez_port_szeregowy(Sciany_Labiryntu)
 basic.forever(function () {
     if (Set_Up_Target == 0) {
         led.plot(Labirynt_X, 4 - Labirynt_Y)
